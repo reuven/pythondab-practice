49cc447660c6fb98e1de41c9204200013016891e
Michael Seidel <mseidel@gmail.com>
2023-01-24 18:42:04 -0600

    Exercise 2 code for Mike's menu.py

---
+++ menu.py
@@ -0,0 +1,24 @@
+'''
+Mike's menu.py for the modules lessons
+
+This function should take any number of string arguments,
+    which are then presented to the user.
+
+'''
+import sys
+
+def menu(*args):
+    while True:
+        my_input = input(f"Enter your choice from {args}: ").strip()
+
+        if not my_input:
+            break
+
+        if my_input in args:
+            return my_input
+        else:
+            print(f'Your choice is not in the list: {my_input}')
+
+if __name__ == '__main__':
+    choice = menu(*sys.argv[1:])
+    print(f'Your choice was -->  {choice}')
